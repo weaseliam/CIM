c2644d2c9678e367590b2824fadb23be83f6552c
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2017-03-11 22:07:30 +0200

    tweaked maven configuration

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CIM
+# CIM project
+
+### How to build project
+Required tools
+* Git
+* JDK8
+* Maven
+
+Clone project.
+Build it with `mvn clean install` in project root.
+Run it with `java -jar [web-application].war` in _web/target_ folder.
+Check it out at `http://localhost:8080`
--- core/src/main/java/com/cim/core/rest/AppRest.java
+++ core/src/main/java/com/cim/core/rest/AppRest.java
@@ -1,14 +1,14 @@
 package com.cim.core.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AppRest
 {
     @RequestMapping("/")
-    String home() 
+    String toIndex() 
 	{
-        return "Hello World !";
+        return "index.html";
     }
 }
