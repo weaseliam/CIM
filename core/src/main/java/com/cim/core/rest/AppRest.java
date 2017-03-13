6eefdedf4fca95503f3613e8fc07e874c3952a52
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2017-03-13 21:42:14 +0200

    added log4j2 logging system

--- core/src/main/java/com/cim/core/rest/AppRest.java
+++ core/src/main/java/com/cim/core/rest/AppRest.java
@@ -1,14 +1,20 @@
 package com.cim.core.rest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppRest
 {
+	private static final Logger logger = LogManager.getLogger(AppRest.class);
+	
     @RequestMapping("/")
     String toIndex() 
 	{
+    	logger.debug("Returning index.html for application root");
+    	
         return "index.html";
     }
 }
--- web/src/main/java/com/cim/web/CIMApplication.java
+++ web/src/main/java/com/cim/web/CIMApplication.java
@@ -1,5 +1,7 @@
 package com.cim.web;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +10,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class CIMApplication 
 {
+	private static final Logger logger = LogManager.getLogger(CIMApplication.class);
+	
     public static void main(String[] args) throws Exception 
 	{
+    	logger.info("Staring application from Spring Boot main");
+    	
         SpringApplication.run(CIMApplication.class, args);
     }
 }
