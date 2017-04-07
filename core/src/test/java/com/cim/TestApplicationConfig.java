1a3919843a9cd7de635825f66fed873391396af6
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2017-04-08 00:25:25 +0300

    finished integrating rest-assured for REST testing

---
+++ core/src/test/java/com/cim/TestApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.cim;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ComponentScan("com.cim")
+@EnableJpaRepositories("com.cim")
+@EntityScan("com.cim")
+@SpringBootApplication
+public class TestApplicationConfig 
+{
+}
--- core/src/test/java/com/cim/core/rest/AppRestTest.java
+++ core/src/test/java/com/cim/core/rest/AppRestTest.java
@@ -1,6 +1,7 @@
 package com.cim.core.rest;
 
-import org.junit.Before;
+import static io.restassured.RestAssured.given;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
@@ -8,25 +9,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.cim.web.TestCIMApplication;
-
-import io.restassured.RestAssured;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = TestCIMApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AppRestTest
 {
     @LocalServerPort
     int port;
 	
-    @Before
-    public void setUp() 
-    {
-        RestAssured.port = port;
-    }
-    
 	@Test
-	public void testAppIndex()
+	public void testAppRoot()
 	{
+		given().
+			port(port).
+		when().
+			get("/");
 	}
 }
