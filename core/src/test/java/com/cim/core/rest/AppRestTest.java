54a7e48f8e95c397666c479337da535d846ee1fd
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2017-04-07 20:50:38 +0300

    integrate rest-assured for testing take 1

--- core/src/test/java/com/cim/core/rest/AppRestTest.java
+++ core/src/test/java/com/cim/core/rest/AppRestTest.java
@@ -1,5 +1,32 @@
 package com.cim.core.rest;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cim.web.TestCIMApplication;
+
+import io.restassured.RestAssured;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestCIMApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AppRestTest
 {
+    @LocalServerPort
+    int port;
+	
+    @Before
+    public void setUp() 
+    {
+        RestAssured.port = port;
+    }
+    
+	@Test
+	public void testAppIndex()
+	{
+	}
 }
