f63aabb1c65354bb2a5bcc0e0ff3709924191c5b
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2018-02-02 19:38:11 +0200

    refactor tools application

--- tools/src/main/java/com/cim/tools/CIMTools.java
+++ tools/src/main/java/com/cim/tools/CIMTools.java
@@ -5,13 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("com.cim")
-@EnableJpaRepositories("com.cim")
-@EntityScan("com.cim")
 @SpringBootApplication
 public class CIMTools implements CommandLineRunner
 {
@@ -20,13 +16,11 @@ public class CIMTools implements CommandLineRunner
 	@Override
 	public void run(String... args) throws Exception
 	{
-		log.debug("Execute command {}", (Object[]) args);
+		log.debug("Execute command '{}'", (Object[]) args);
 	}
 
 	public static void main(String[] args) throws Exception
 	{
-		log.info("Staring tools application");
-
 		SpringApplication.run(CIMTools.class, args);
 	}
 }
