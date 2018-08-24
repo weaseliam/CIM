289b6523ba98777ba4a60e72091f881548834a3b
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2018-08-24 15:21:07 +0300

    proper loading state management

--- ui/src/app/app-reducer.js
+++ ui/src/app/app-reducer.js
@@ -7,9 +7,17 @@ import {
 
 export const INITIAL_STATE = {
   session: null,
-  loading: false,
-  loadingContent: false,
+  loading: 0,
+  loadingContent: 0,
   bootstrap: false
+};
+
+const newLoading = (currentLoading, increment) => {
+  if (currentLoading === 0 && increment === -1) {
+    return 0;
+  }
+
+  return currentLoading + increment;
 };
 
 export const appReducer = (state = INITIAL_STATE, action) => {
@@ -28,17 +36,25 @@ export const appReducer = (state = INITIAL_STATE, action) => {
         bootstrap: true
       };
 
-    case setAppLoadingContentAction.SUCCESS:
-      return {
-        ...state,
-        loadingContent: action.payload
-      };
+    case setAppLoadingContentAction.SUCCESS: {
+      const currentLoading = state.loadingContent;
+      const increment = action.payload ? 1 : -1;
 
-    case setAppLoadingAction.SUCCESS:
       return {
         ...state,
-        loading: action.payload
+        loadingContent: newLoading(currentLoading, increment)
       };
+    }
+
+    case setAppLoadingAction.SUCCESS: {
+      const currentLoading = state.loading;
+      const increment = action.payload ? 1 : -1;
+
+      return {
+        ...state,
+        loading: newLoading(currentLoading, increment)
+      };
+    }
 
     default:
       return state;
--- ui/src/app/app-selector.js
+++ ui/src/app/app-selector.js
@@ -2,10 +2,10 @@ const getRoot = state =>
   state.app;
 
 export const appLoadingContentSelector = state =>
-  getRoot(state).loadingContent;
+  getRoot(state).loadingContent > 0;
 
 export const appLoadingSelector = state =>
-  getRoot(state).loading;
+  getRoot(state).loading > 0;
 
 export const appUserSelector = state =>
   getRoot(state).session.user;
