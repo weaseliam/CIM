a4cc280291d3348ae70c6877e2c4c5f352217ef3
Laurentiu Dimana <l_a_u_r_d@yahoo.com>
2019-04-08 18:19:10 +0300

    better maven configuration for dev environment

--- core/src/main/java/com/cim/core/contract/Contract.java
+++ core/src/main/java/com/cim/core/contract/Contract.java
@@ -10,8 +10,7 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
-
-	public class Contract
+public class Contract
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,212 +41,212 @@ import javax.validation.constraints.NotNull;
 	private Integer			codCs;
 	private Integer			matricola;
 	private String			modif;
-	
+
 	public Long getId()
 	{
 		return id;
 	}
-	
+
 	public void setId(Long id)
 	{
 		this.id = id;
 	}
-	
+
 	public Long getGraveownerId()
 	{
 		return graveownerId;
 	}
-	
+
 	public void setGraveownerId(Long graveownerId)
 	{
 		this.graveownerId = graveownerId;
 	}
-	
+
 	public Long getGraveId()
 	{
 		return graveId;
 	}
-	
+
 	public void setGraveId(Long graveId)
 	{
 		this.graveId = graveId;
 	}
-	
+
 	public Long getExemptId()
 	{
 		return exemptId;
 	}
-	
+
 	public void setExemptId(Long exemptId)
 	{
 		this.exemptId = exemptId;
 	}
-	
+
 	public Long getOldId()
 	{
 		return oldId;
 	}
-	
+
 	public void setOldId(Long oldId)
 	{
 		this.oldId = oldId;
 	}
-	
+
 	public Integer getAni()
 	{
 		return ani;
 	}
-	
+
 	public void setAni(Integer ani)
 	{
 		this.ani = ani;
 	}
-	
+
 	public LocalDateTime getDataIncep()
 	{
 		return dataIncep;
 	}
-	
+
 	public void setDataIncep(LocalDateTime dataIncep)
 	{
 		this.dataIncep = dataIncep;
 	}
-	
+
 	public LocalDateTime getDataExp()
 	{
 		return dataExp;
 	}
-	
+
 	public void setDataExp(LocalDateTime dataExp)
 	{
 		this.dataExp = dataExp;
 	}
-	
+
 	public String getNrCh()
 	{
 		return nrCh;
 	}
-	
+
 	public void setNrCh(String nrCh)
 	{
 		this.nrCh = nrCh;
 	}
-	
+
 	public LocalDateTime getDataCh()
 	{
 		return dataCh;
 	}
-	
+
 	public void setDataCh(LocalDateTime dataCh)
 	{
 		this.dataCh = dataCh;
 	}
-	
+
 	public BigDecimal getSuma()
 	{
 		return suma;
 	}
-	
+
 	public void setSuma(BigDecimal suma)
 	{
 		this.suma = suma;
 	}
-	
+
 	public String getContract()
 	{
 		return contract;
 	}
-	
+
 	public void setContract(String contract)
 	{
 		this.contract = contract;
 	}
-	
+
 	public String getNrContr()
 	{
 		return nrContr;
 	}
-	
+
 	public void setNrContr(String nrContr)
 	{
 		this.nrContr = nrContr;
 	}
-	
+
 	public LocalDateTime getDataContr()
 	{
 		return dataContr;
 	}
-	
+
 	public void setDataContr(LocalDateTime dataContr)
 	{
 		this.dataContr = dataContr;
 	}
-	
+
 	public Integer getInd()
 	{
 		return ind;
 	}
-	
+
 	public void setInd(Integer ind)
 	{
 		this.ind = ind;
 	}
-	
+
 	public String getStare()
 	{
 		return stare;
 	}
-	
+
 	public void setStare(String stare)
 	{
 		this.stare = stare;
 	}
-	
+
 	public LocalDateTime getDataStare()
 	{
 		return dataStare;
 	}
-	
+
 	public void setDataStare(LocalDateTime dataStare)
 	{
 		this.dataStare = dataStare;
 	}
-	
+
 	public Integer getTranse()
 	{
 		return transe;
 	}
-	
+
 	public void setTranse(Integer transe)
 	{
 		this.transe = transe;
 	}
-	
+
 	public Integer getCodCs()
 	{
 		return codCs;
 	}
-	
+
 	public void setCodCs(Integer codCs)
 	{
 		this.codCs = codCs;
 	}
-	
+
 	public Integer getMatricola()
 	{
 		return matricola;
 	}
-	
+
 	public void setMatricola(Integer matricola)
 	{
 		this.matricola = matricola;
 	}
-	
+
 	public String getModif()
 	{
 		return modif;
 	}
-	
+
 	public void setModif(String modif)
 	{
 		this.modif = modif;
